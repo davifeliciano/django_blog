85d2e8be22e743b0e660aa983deff394f61bb2b9
Davi Feliciano <dfeliciano37@gmail.com>
2022-10-20 18:45:10 -0300

    Adding the user login system

--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpRequest, HttpResponse
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm
 
 
@@ -10,8 +11,16 @@ def register(request: HttpRequest) -> HttpResponse:
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get("username")
-            messages.success(request, f"Account created for user {username}")
+            messages.success(
+                request,
+                f"Account created for user {username}! You are now able to login",
+            )
             return redirect("blog-home")
     else:
         form = UserRegisterForm()
     return render(request, "users/register.html", context={"form": form})
+
+
+@login_required
+def profile(request: HttpRequest) -> HttpResponse:
+    return render(request, "users/profile.html")
